a6f6e16c593b18ce00978262662ab5c7ed97876d
Pavel Larionov <pavel.larionov@cern.ch>
2025-11-26 23:52:09 +0100

    Mask IRIS L2

--- Examples/Algorithms/Fatras/src/FatrasSimulation.cpp
+++ Examples/Algorithms/Fatras/src/FatrasSimulation.cpp
@@ -54,7 +54,7 @@ struct HitSurfaceSelector {
     // Get the geometry ID
     const auto geoID = surface.geometryId();
     // Define sensors to reject
-    bool rejectSensor = (geoID.volume() == 13 && geoID.layer() == 4);
+    bool rejectSensor = (geoID.volume() == 13 && geoID.layer() == 6);
     // Reject specific sensors
     if (rejectSensor) {
       // std::cout << "Rejecting hit on surface with ID: vol="
